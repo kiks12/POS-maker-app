9fdcd792f21d03b123286989277d18383ca102af
JMST23 <jmtolentino23.jmt@gmail.com>
2021-06-30 14:15:15 +0800

    Header Component added

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,24 @@
+/* index page dependencies */
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Header from "../Components/Header";
 
+/* index page instance */
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
-      <div className={styles.container}>
-        <div className='card'>
-          <h1>Hello from POS maker</h1>
-          <button>Hello</button>
+      <main>
+        <Header />
+        <div className={styles.container}>
+          <div className='card'>
+            <h1>Hello from POS maker</h1>
+            <button>Hello</button>
+          </div>
         </div>
-      </div>
+      </main>
     </>
   );
 }
