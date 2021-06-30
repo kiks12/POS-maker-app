70a52f26d1645184bf2c6f321bad4c52ce838cd4
JMST23 <jmtolentino23.jmt@gmail.com>
2021-07-01 07:49:11 +0800

    next-auth uninstalled

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
 /* app global dependencies */
 import "../styles/globals.css";
-import { Provider } from "next-auth/client";
 
+/* my app instance - whole application */
 function MyApp({ Component, pageProps }) {
-  return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,15 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Header from "../Components/Header";
-import { useSession, signIn, signOut } from "next-auth/client";
 
 /* index page instance */
 export default function Home() {
-  const [session] = useSession();
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
       <main>
-        <p>{session ? "logged in" : "logged out"}</p>
-        {session ? (
-          <button onClick={() => signOut}>Log out.</button>
-        ) : (
-          <button onClick={signIn}>Log in.</button>
-        )}
         <Header />
         <div className='container'>
           <div className='card'>
