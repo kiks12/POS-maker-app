3c12312349c6f8c178c9236fa274d4ebb1be11b8
JMST23 <jmtolentino23.jmt@gmail.com>
2021-06-30 10:59:23 +0800

    login page added

---
+++ pages/login/index.js
@@ -0,0 +1,10 @@
+/* login page dependencies */
+import styles from "../../styles/Home.module.css";
+
+/* login page instance */
+const login = () => {
+  return <div>Hello</div>;
+};
+
+/* export login page */
+export default login;
