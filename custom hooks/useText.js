8b0b38b80a8c8eb49b666ecba29554f2795065d4
JMST23 <jmtolentino23.jmt@gmail.com>
2021-06-30 13:49:26 +0800

    custom hook for input handling

--- custom hooks/useText.js
+++ custom hooks/useText.js
@@ -1,5 +1,7 @@
+/* useText dependencies */
 import { useState } from "react";
 
+/* useText function */
 export const useText = (initialValue) => {
   const [values, setValues] = useState(initialValue);
   const setValueFunction = (e) => {
