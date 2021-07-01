db92e862e0b5a430fe626c33c99586c6d9cf562b
JMST23 <jmtolentino23.jmt@gmail.com>
2021-07-01 08:11:47 +0800

    passport js configuration

---
+++ config/passport.js
@@ -0,0 +1,17 @@
+/* passport configuration */
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      console.log(profile);
+      return done(err);
+    }
+  )
+);
