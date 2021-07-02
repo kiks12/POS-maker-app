feefcd8095ff36065da90406d62f0fa16d3498c5
JMST23 <jmtolentino23.jmt@gmail.com>
2021-07-02 08:06:59 +0800

    passport js configuration

--- config/passport.js
+++ config/passport.js
@@ -2,16 +2,17 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 
+/* passport strategy using google oauth 2 */
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
+      callbackURL: "http://localhost:3000/api/auth/google/callback",
     },
     function (accessToken, refreshToken, profile, done) {
       console.log(profile);
-      return done(err);
+      return done();
     }
   )
 );
--- pages/api/auth/google/index.js
+++ pages/api/auth/google/index.js
@@ -5,5 +5,7 @@ import nextConnect from "next-connect";
 const handler = nextConnect();
 
 export default handler.get(
-  passport.authenticate("google", { scope: "https://www.google.com/m8/feeds" })
+  passport.authenticate("google", {
+    scope: ["https://www.google.com/m8/feeds", "email", "profile"],
+  })
 );
