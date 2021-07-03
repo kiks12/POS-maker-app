c69e218f95e0cde1720708b30930c65280c4be4a
JMST23 <jmtolentino23.jmt@gmail.com>
2021-07-03 11:06:57 +0800

    session fetching working

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,18 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Header from "../Components/Header";
+import { applySession } from "next-session";
 
 /* index page instance */
-export default function Home() {
+export default function Home({ user }) {
+  const { displayName } = user;
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
       <main>
-        <Header />
+        <Header username={displayName} />
         <div className='container'>
           <div className='card'>
             <h1>Hello from POS maker</h1>
@@ -22,3 +24,17 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps = async ({ req, res }) => {
+  try {
+    await applySession(req, res);
+    const userInformation = req.session.passport.user;
+    return {
+      props: {
+        user: userInformation,
+      },
+    };
+  } catch (err) {
+    console.error(err);
+  }
+};
